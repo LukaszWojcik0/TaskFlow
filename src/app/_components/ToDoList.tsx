c5f65c3c28fd09cbf0da4b530ce016ecf11ab45a
LukaszWojcik3B <wulkasz0@gmail.com>
2024-09-16 19:45:21 +0200

    ✅localStorage
    deleting tasks added

--- src/app/_components/ToDoList.tsx
+++ src/app/_components/ToDoList.tsx
@@ -13,8 +13,20 @@ import {
   DialogFooter,
   DialogClose,
 } from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "@/components/ui/input";
+import Image from "next/image";
 
 export function ToDoList() {
   const { addTask } = useTasks();
@@ -48,15 +60,34 @@ export function ToDoList() {
     addTask(newTask);
   };
 
+  const handleDeleteTask = (taskId: string) => {
+    const oldTasks = localStorage.getItem("tasks");
+    if (oldTasks) {
+      const parsedTasks = JSON.parse(oldTasks);
+      const taskNumId = Number(taskId);
+      if (Array.isArray(parsedTasks)) {
+        const updatedTasks = parsedTasks.filter(
+          (task: { id: number }) => task.id !== taskNumId
+        );
+
+        updateLocalStorage(updatedTasks);
+        setLocalTasks(updatedTasks);
+      }
+    }
+  };
+
   return (
     <>
       <Card className="h-screen w-full">
         <CardTitle className="p-4 flex">Your tasks:</CardTitle>
 
         <Dialog>
-          <DialogTrigger className="px-3">
-            <Button>Add Task</Button>
-          </DialogTrigger>
+          <div className="px-3">
+            <DialogTrigger asChild>
+              <Button className="px-5">Add Task</Button>
+            </DialogTrigger>
+          </div>
+
           <DialogContent>
             <DialogHeader>
               <DialogTitle>Name your task:</DialogTitle>
@@ -91,10 +122,52 @@ export function ToDoList() {
         </Dialog>
         <ul>
           {localTasks.map((task) => (
-            <li key={task.id} className="p-4 border-b">
-              <div className="font-bold">{task.title}</div>
-              <div>{task.description}</div>
-              {/* <img></img> later to be "delete task" icon */}
+            <li key={task.id} className="p-4 border-b flex">
+              <div className="w-10/12">
+                <div className="font-bold">{task.title}</div>
+                <div>{task.description}</div>
+              </div>
+
+              <AlertDialog>
+                <AlertDialogTrigger asChild>
+                  <Image
+                    src="assets/delete-svgrepo-com.svg"
+                    width={30}
+                    height={30}
+                    alt="delete-img"
+                    className="hover:cursor-pointer"
+                  ></Image>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you sure u want to delete this task?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      This action cannot be undone. This will permanently delete
+                      your account and remove your data from our servers.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction
+                      onClick={() => {
+                        handleDeleteTask(String(task.id));
+                      }}
+                    >
+                      Continue
+                    </AlertDialogAction>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+
+              <Image
+                src="assets/calendar-add-svgrepo-com.svg"
+                width={30}
+                height={30}
+                alt="add-calendar-img"
+                className="hover:cursor-pointer"
+              ></Image>
               {/* <img></img> later to be "add to calendar" icon */}
             </li>
           ))}
