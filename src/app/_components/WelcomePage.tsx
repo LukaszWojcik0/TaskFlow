bad5022e2f9c0c4c4465e20974fdc978a391875f
Łukasz Wójcik <wulkasz0@gmail.com>
2024-09-17 13:57:56 +0200

    Cleanup and WelcomePage works

---
+++ src/app/_components/WelcomePage.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/components/ui/card";
+
+export function WelcomePage() {
+  return (
+    <div className="absolute z-10 h-full w-full p-24">
+      <Card className="p-4">
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+      </Card>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,76 +1,24 @@
 "use client";
 
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-
 import Calendar from "@/app/_components/Calendar";
 import { ToDoList } from "@/app/_components/ToDoList";
 import Navbar from "./_components/Navbar";
+import { WelcomePage } from "./_components/WelcomePage";
 
 function Home() {
-  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
-  const [isToDoListVisible, setToDoListVisible] = useState(false);
-
-  const handleClickCalendar = () => {
-    setIsCalendarVisible(!isCalendarVisible);
-  };
-  const handleClickToDoList = () => {
-    setToDoListVisible(!isToDoListVisible);
-  };
-
   return (
-    <>
-      {/* <Card>
-        <CardHeader>
-          <CardTitle>TaskFlow dev process</CardTitle>
-          <CardDescription>Chose ur path</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Button
-            className="mx-5"
-            variant="outline"
-            onClick={() => {
-              handleClickCalendar();
-              if (isToDoListVisible) {
-                handleClickToDoList();
-              }
-            }}
-          >
-            Calendar
-          </Button>
-          <Button
-            variant="outline"
-            onClick={() => {
-              handleClickToDoList();
-              if (isCalendarVisible) {
-                handleClickCalendar();
-              }
-            }}
-          >
-            ToDo List
-          </Button>
-        </CardContent>
-      </Card> */}
+    <div>
       <Navbar />
-      {/* {isCalendarVisible && ( */}
+      {/* <WelcomePage /> */}
       <div className="flex">
-        <div className="w-1/4 ">
+        <div className="w-1/4">
           <ToDoList />
         </div>
         <div className="w-3/4">
           <Calendar />
         </div>
       </div>
-      {/* )} */}
-      {isToDoListVisible && <ToDoList />}
-    </>
+    </div>
   );
 }
 
